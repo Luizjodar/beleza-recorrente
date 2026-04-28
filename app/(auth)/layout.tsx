c04e09730bf50dd90289070c6f30a1659e5dbd37
Luizjodar <luisjodar00@gmail.com>
2026-04-28 18:03:21 -0300

    Add AuthLayout component with TemaProvider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,1 +1,10 @@
+import { TemaProvider } from '../lib/tema'
+import Layout from '../components/Layout'
 
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <TemaProvider>
+      <Layout>{children}</Layout>
+    </TemaProvider>
+  )
+}
